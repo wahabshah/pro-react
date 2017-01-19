5b11b643c2ed1d1d99a10eda9d4bd43452949847
Wahab <wahabshah@hotmail.com>
2017-01-19 00:23:12 -0800

    react-router added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from "react-router";
 import About from './About';
 import Home from './Home';
 import Repos from './Repos';
@@ -7,35 +8,20 @@ import Repos from './Repos';
 export default class App extends Component {
   constructor(){
      super(...arguments);
-     this.state= {route: window.location.hash.substr(1)};
   }
-  componentDidMount(){
-       window.addEventListener("hashchange",()=>{
-             this.setState({route: window.location.hash.substr(1)})
-       })
-  }
+  
   render() {
-    var Comp;
-    switch(this.state.route){
-      case "/about":
-       Comp= About;
-       break;
-      case "/repos":
-       Comp= Repos;
-       break;
-      default:
-       Comp=Home;
-    }
+    
     return (
       <div>
       <header>App</header>
       <menu>
       <ul>
-         <li><a href="#/repos">Repos</a></li>
-         <li><a href="#/about">About</a></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/repos">Repos</Link></li>
       </ul>
       </menu>
-      <Comp/>
+      {this.props.children}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router,Route,IndexRoute,browserHistory} from 'react-router';
 import App from './App';
+import About from './About';
+import Repos from './Repos';
+import Home from './Home';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(      
+    (<Router history={browserHistory}>
+         <Route path="/" component={App}>
+            <IndexRoute component={Home}/>
+            <Route path="about" component={About}/>
+            <Route path="repos" component={Repos}/>
+         </Route>
+    </Router>)
+      , document.getElementById('root'));
